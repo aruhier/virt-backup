26c4afdfcc5edbf94d410b5947ea413c13f28c5e
Anthony Ruhier <anthony.ruhier@gmail.com>
2016-09-12 23:23:41 +0200

    Fix travis issues

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 
     keywords="libvirt",
     packages=["virt_backup", ],
-    install_requires=["defusedxml", "libvirt-python", "tqdm"],
+    install_requires=["defusedxml", "libvirt-python", "lxml", "tqdm"],
     setup_requires=['pytest-runner', ],
     tests_require=['pytest', 'pytest-cov', "pytest-mock", "pytest-xdist"],
     entry_points={
